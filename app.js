ee1497190f0e775021513192bc6e8c52ac89139e
GiulianoMoyanoMuscari <moyanomuscarigiuliano@gmail.com>
2023-11-06 19:54:53 -0300

    Creadas vistas login.html y register.html con sus respectivas rutas en express

--- app.js
+++ app.js
@@ -15,5 +15,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './views/home.html'))
 });
 
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/register.html'))
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/login.html'))
+});
+
+
+
 // Dejamos Estaticos a los recursos de la carpeta Public //
 app.use(express.static(pathPublic))
